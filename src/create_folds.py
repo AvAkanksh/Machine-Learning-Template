5e7245fc02dd1a11d8d09cf902a3efd40fb4c745
Akanksh <akanksh@localhost.localdomain>
2021-06-01 02:41:34 +0530

    updating the files

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,17 +1,7 @@
 import pandas as pd
-from sklearn import model_selection
+from sklearn.model_selection import train_test_split
 
-
-if __name__== "__main__":
-    df = pd.read_csv('input/train.csv')
+if __name__ == '__main__':
+    df = pd.read_csv('../input/Categorical Feature Encoding Challenge/train.csv')
     df['kfold'] = -1
-    
     df = df.sample(frac=1).reset_index(drop = True)
-
-    kf = model_selection.StratifiedKFold(n_splits=5,shuffle= True , random_state=42)
-
-    for fold , (train_idx,val_idx) in enumerate(kf.split(df, df.target.values)):
-        print(len(train_idx),len(val_idx))
-        df.loc[val_idx,'kfold'] = fold
-    
-    df.to_csv('input/train_fold.csv', index = False)
